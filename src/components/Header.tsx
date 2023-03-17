4a35789de42808be14c990f36838fea73c5a812d
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 12:16:19 +0100

    initial setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,32 @@
 const Header = () => {
+
+    const path = ['Home', 'About Us', 'Pricing'] as const
     
     return (
-        <div>
-
+      <div>
+        <div className="radioBox">
+          <>
+            {path.map((item) => {
+              return (
+                <Fragment key={item}>
+                  <input
+                    type="radio"
+                    name="history"
+                    id={item}
+                    checked={item === pathToSwitch}
+                    className="hidden"
+                    onChange={() => setPathToSwitch(item)}
+                  />
+                  <label htmlFor={item} className={`capitalize`}>
+                    {item}
+                  </label>
+                </Fragment>
+              );
+            })}
+          </>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Header

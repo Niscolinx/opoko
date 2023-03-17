ec6fcf25fe7e06bb0e3190913e5ab735e1a0b42a
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 17:02:10 +0100

    working on about us

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div className="grid gap-8">
+      <p className="font-bold text-[5rem]">
+        The one-stop shop for all things law
+      </p>
+      <p>
+        Opoko is a legal research tool designed to provide users with relevant
+        laws, case law, and international events. It caters to legal
+        professionals, in-house attorneys, and business enthusiasts who require
+        up-to-date legal information. The platform is a comprehensive source of
+        data on specific practice areas, making it a useful tool for conducting
+        legal research on the go. With Opoko, users have access to a vast
+        library of information that covers a range of legal topics. This
+        includes laws, regulations, and case law from different jurisdictions
+        around the world. The platform also provides users with useful insights
+        into legal issues, making it an essential tool for anyone who requires
+        the latest legal information. Overall, Opoko is a valuable resource for
+        anyone who needs to stay up-to-date on legal developments. Its
+        mobile-friendly design and comprehensive database make it an ideal tool
+        for legal professionals, in-house attorneys, and business enthusiasts
+        alike. Whether you need to research specific legal issues or stay
+        abreast of the latest legal developments, Opoko has everything you need
+        to stay informed.
+      </p>
+    </div>
+  );
 }
 
 export default AboutUs

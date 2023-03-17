560d83af34d13969ebdf6ad3139462be4a6530ee
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 12:17:34 +0100

    initial setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,34 @@
+import { Fragment, useState } from 'react';
+
 const Header = () => {
+  const path = ['Home', 'About Us', 'Pricing'] as const;
+  const [pathToSwitch, setPathToSwitch] = useState('');
 
-    const path = ['Home', 'About Us', 'Pricing'] as const
-    
-    return (
-      <div>
-        <div className="radioBox">
-          <>
-            {path.map((item) => {
-              return (
-                <Fragment key={item}>
-                  <input
-                    type="radio"
-                    name="history"
-                    id={item}
-                    checked={item === pathToSwitch}
-                    className="hidden"
-                    onChange={() => setPathToSwitch(item)}
-                  />
-                  <label htmlFor={item} className={`capitalize`}>
-                    {item}
-                  </label>
-                </Fragment>
-              );
-            })}
-          </>
-        </div>
+  return (
+    <div>
+      <div className="radioBox">
+        <div>
+          {path.map((item) => {
+            return (
+              <Fragment key={item}>
+                <input
+                  type="radio"
+                  name="history"
+                  id={item}
+                  checked={item === pathToSwitch}
+                  className="hidden"
+                  onChange={() => setPathToSwitch(item)}
+                />
+                <label htmlFor={item} className={`capitalize`}>
+                  {item}
+                </label>
+              </Fragment>
+            );
+          })}
+        </>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Header
+export default Header;

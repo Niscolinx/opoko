0db3291509cc6808bc380236da070d4ac9722215
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 15:02:00 +0100

    working on header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 
 function Home() {
   return (
-    <div>
+    <div className="grid grid-cols-2">
       <div>
-        <p className="text-[5.6rem] font-bold">Get access to <span className="text-color-orange">the law</span> in just a few clicks</p>
+        <p className="text-[5.6rem] font-medium">Get access to <span className="text-color-orange">the law</span> in just a few clicks</p>
       </div>
       <div>
         <img alt="" src="/asset/hero.svg" />

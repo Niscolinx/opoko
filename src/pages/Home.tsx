a72e4ca7cd557399c0a1600591f5759c226b2a32
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 14:56:43 +0100

    working on header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
     <div>
-        
+      <div></div>
+      <div>
+        <img alt="" src="/asset/hero.svg" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

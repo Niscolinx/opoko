8919cab580412f15e16a65a963fd0e36e50562bc
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 15:19:05 +0100

    working on header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,12 @@ function Home() {
           Get access to <span className="text-color-orange capitalize font-bold">The Law</span> in
           just a few clicks
         </p>
-        <p className='w-[2.2rem]'>
+        <p className='text-[2.2rem]'>
           Opoko is a legal technology solution that offers insight into specific
           areas of law in triples.
         </p>
         <div className="flex justify-start">
-          <button className="bg-color-blue text-white flex items-center gap-4 px-8 py-4 rounded-2xl ">
+          <button className="bg-color-blue text-white flex items-center gap-4 px-8 py-4 rounded-2xl text-[2.4rem] ">
             Get Started <HiOutlineArrowNarrowRight />{' '}
           </button>
         </div>

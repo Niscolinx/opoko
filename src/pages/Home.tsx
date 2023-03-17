948f34522708bf6dcb87c0c84f9c5bd46c3a269b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 13:37:51 +0100

    initial setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Auth from '../container/Auth';
 
@@ -6,6 +7,9 @@ function Home() {
     <div className="px-16">
       <Header />
 
+      <div>
+        <Outlet />
+      </div>
       <Auth />
     </div>
   );

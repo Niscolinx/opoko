2e8ed740119e2c550d0801fdd0e02415d918a2a7
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 13:47:08 +0100

    working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,6 @@ const Header = () => {
   const path = ['home', 'about-us', 'pricing'] as const;
   const [pathToSwitch, setPathToSwitch] = useState('home');
 
-  console.log({ pathToSwitch });
-
   return (
     <div className="flex items-center justify-between">
       <img src="/asset/logo.svg" alt="" className="w-[12rem]" />
@@ -15,27 +13,16 @@ const Header = () => {
           console.log({ item, pathToSwitch });
           return (
             <Link key={item} to={`/${item}`}>
-              <>
-                <input
-                  type="radio"
-                  name="header"
-                  id={item}
-                  checked={item === pathToSwitch}
-                  className="hidden"
-                  onChange={() => setPathToSwitch(item)}
-                />
-                <label
-                  htmlFor={item}
-                  className={`capitalize  cursor-pointer grid gap-2 justify-items-center`}
-                >
-                  {item.replace('-', ' ')}
-                  {item === pathToSwitch && (
-                    <span className="bg-color-orange w-[1rem] h-[1rem] rounded-full">
-                      &nbsp;
-                    </span>
-                  )}
-                </label>
-              </>
+              <div
+                className={`capitalize  cursor-pointer grid gap-2 justify-items-center`}
+              >
+                {item.replace('-', ' ')}
+                {item === pathToSwitch && (
+                  <span className="bg-color-orange w-[1rem] h-[1rem] rounded-full">
+                    &nbsp;
+                  </span>
+                )}
+              </div>
             </Link>
           );
         })}

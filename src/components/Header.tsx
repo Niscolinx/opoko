167f1569c5259f1026164ba0523667297de80da4
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 12:41:16 +0100

    initial setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,29 +5,28 @@ const Header = () => {
   const [pathToSwitch, setPathToSwitch] = useState('home');
 
   return (
-    <div className=''>
-        <img src="/asset/logo.svg" alt="" className='w-[12rem]' />
+    <div className="flex items-center">
+      <img src="/asset/logo.svg" alt="" className="w-[12rem]" />
       <div className="radioBox">
-      
-          {path.map((item) => {
-            return (
-              <Fragment key={item}>
-                <input
-                  type="radio"
-                  name="header"
-                  id={item}
-                  checked={item === pathToSwitch}
-                  className="hidden"
-                  onChange={() => setPathToSwitch(item)}
-                />
-                <label htmlFor={item} className={`capitalize`}>
-                  {item}
-                </label>
-              </Fragment>
-            );
-          })}
-    
+        {path.map((item) => {
+          return (
+            <Fragment key={item}>
+              <input
+                type="radio"
+                name="header"
+                id={item}
+                checked={item === pathToSwitch}
+                className="hidden"
+                onChange={() => setPathToSwitch(item)}
+              />
+              <label htmlFor={item} className={`capitalize`}>
+                {item}
+              </label>
+            </Fragment>
+          );
+        })}
       </div>
+      <button>Login</button>
     </div>
   );
 };

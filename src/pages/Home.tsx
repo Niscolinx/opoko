68193668a90f154b9f7c1c9dc84b2d684f89cf0d
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 12:19:02 +0100

    initial setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
+import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Auth from '../container/Auth';
 
 function Home() {
   return (
     <div className="app">
-
+      <Header />
       <Hero />
 
       <Auth />

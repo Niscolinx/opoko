b66bc02d18ad66b9e38828b2cbae272ff41d1cb8
Igboanugwo Collins <munisco12@gmail.com>
2023-03-17 15:09:53 +0100

    working on header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
 function Home() {
   return (
     <div className="grid grid-cols-2 items-center gap-8">
       <div>
-        <p className="text-[5rem] font-medium capitalize">Get access to <span className="text-color-orange">the law</span> in just a few clicks</p>
+        <p className="text-[5rem] font-medium capitalize">
+          Get access to <span className="text-color-orange">the law</span> in
+          just a few clicks
+        </p>
+        <p>
+          Opoko is a legal technology solution that offers insight into specific
+          areas of law in triples.
+        </p>
+        <button className='bg-color-blue text-white flex items-center gap-4 btn '>
+          Get Started <HiOutlineArrowNarrowRight />{' '}
+        </button>
       </div>
       <div>
         <img alt="" src="/asset/hero.svg" />

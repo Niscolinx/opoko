938f922ab03607d36c039f9bf2e48cf60c88d435
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 11:21:32 +0100

    working on pricing

--- src/pages/billing/steps/First.tsx
+++ src/pages/billing/steps/First.tsx
@@ -12,9 +12,20 @@ function First() {
       <div>
         <input
           type="email"
-          className="py-4 px-4 border"
+          className="py-4 px-4 border rounded-lg"
           placeholder="Email Address"
         />
+
+        <p className="font-bold">
+          By clicking "Continue", I agree that Opoko can keep me informed by
+          sending personalized emails about products and services. See our
+          Privacy Policy for details or to opt-out at any time.
+        </p>
+
+        <div className='flex items-center gap-4'>
+          <input type="checkbox" id="confirm" />
+          <label htmlFor="confirm">Please don't contact me via email</label>
+        </div>
       </div>
     </div>
   );

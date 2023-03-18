8b3852f069d88779eeb9c692e0e1e141ba2ed359
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 12:40:35 +0100

    working on pricing

--- src/container/Auth.tsx
+++ src/container/Auth.tsx
@@ -26,6 +26,12 @@ const AuthContext = createContext<AuthContext>(null as any);
 
 const useAuthContext = () => {
   const context = useContext(AuthContext)
+
+  if(!context){
+    throw new Error('Auth Context must be used within the Auth Container')
+  }
+
+  return context
 }
 
 function Auth() {

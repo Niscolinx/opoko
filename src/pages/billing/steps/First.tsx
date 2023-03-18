0a32a9bf40fe928a785806800db54e42bfb0c620
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 11:17:52 +0100

    working on pricing

--- src/pages/billing/steps/First.tsx
+++ src/pages/billing/steps/First.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function First() {
   return (
-    <div>First</div>
-  )
+    <div className="grid gap-4">
+      <div>
+        <p className="font-medium text-[2.8rem]">Enter your email address</p>
+        <p className="text-[#4A7AF7] font-bold">
+          We'll create an account if you're new, or ask you to sign in.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default First

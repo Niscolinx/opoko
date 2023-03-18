e4857e1fe47dc8a2600f1e4aac55c5a4ae3e5baa
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 12:08:17 +0100

    working on pricing

--- src/pages/billing/steps/First.tsx
+++ src/pages/billing/steps/First.tsx
@@ -9,24 +9,27 @@ function First() {
           We'll create an account if you're new, or ask you to sign in.
         </p>
       </div>
-      <div>
-        <input
-          type="email"
-          className="py-4 px-4 border rounded-lg"
-          placeholder="Email Address"
-        />
+      <form>
+        <div>
+          <input
+            type="email"
+            className="py-4 px-4 border rounded-lg"
+            placeholder="Email Address"
+          />
 
-        <p className="font-bold">
-          By clicking "Continue", I agree that Opoko can keep me informed by
-          sending personalized emails about products and services. See our
-          Privacy Policy for details or to opt-out at any time.
-        </p>
+          <p className="font-bold">
+            By clicking "Continue", I agree that Opoko can keep me informed by
+            sending personalized emails about products and services. See our
+            Privacy Policy for details or to opt-out at any time.
+          </p>
 
-        <div className='flex items-center gap-4'>
-          <input type="checkbox" id="confirm" />
-          <label htmlFor="confirm">Please don't contact me via email</label>
+          <div className="flex items-center gap-4">
+            <input type="checkbox" id="confirm" />
+            <label htmlFor="confirm">Please don't contact me via email</label>
+          </div>
         </div>
-      </div>
+        <button className='px-16 py-4 rounded-2xl bg-color-blue text-white text-center disabled:opacity-50 disabled:cursor-not-allowed'>Continue</button>
+      </form>
     </div>
   );
 }

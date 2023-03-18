0c5a9d24ad883a427d97065357a3849cdaed2d3b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 12:08:18 +0100

    working on pricing

--- src/pages/billing/Billing.tsx
+++ src/pages/billing/Billing.tsx
@@ -4,23 +4,14 @@ import First from './steps/First';
 function Billing() {
   const [step, setStep] = useState(1);
 
-  const renderPaths = new Map<number, JSX.Element>([
-    [
-      1,
-     <First/>,
-    ],
-  ]);
+  const renderPaths = new Map<number, JSX.Element>([[1, <First />]]);
 
   return (
-    <div
-      className="grid"
-      style={{
-        gridTemplateColumns: '60% auto',
-      }}
-    >
+    <div className="grid">
       <p>
         Step {step} of {renderPaths.size}
       </p>
+      {renderPaths.get(step)}
     </div>
   );
 }
--- src/pages/billing/steps/First.tsx
+++ src/pages/billing/steps/First.tsx
@@ -2,34 +2,41 @@ import React from 'react';
 
 function First() {
   return (
-    <div className="grid gap-4">
-      <div>
-        <p className="font-medium text-[2.8rem]">Enter your email address</p>
-        <p className="text-[#4A7AF7] font-bold">
-          We'll create an account if you're new, or ask you to sign in.
-        </p>
-      </div>
-      <form>
+    <div className=''>
+      <section className="grid gap-4">
         <div>
-          <input
-            type="email"
-            className="py-4 px-4 border rounded-lg"
-            placeholder="Email Address"
-          />
-
-          <p className="font-bold">
-            By clicking "Continue", I agree that Opoko can keep me informed by
-            sending personalized emails about products and services. See our
-            Privacy Policy for details or to opt-out at any time.
+          <p className="font-medium text-[2.8rem]">Enter your email address</p>
+          <p className="text-[#4A7AF7] font-bold">
+            We'll create an account if you're new, or ask you to sign in.
           </p>
-
-          <div className="flex items-center gap-4">
-            <input type="checkbox" id="confirm" />
-            <label htmlFor="confirm">Please don't contact me via email</label>
-          </div>
         </div>
-        <button className='px-16 py-4 rounded-2xl bg-color-blue text-white text-center disabled:opacity-50 disabled:cursor-not-allowed'>Continue</button>
-      </form>
+        <form>
+          <div>
+            <input
+              type="email"
+              className="py-4 px-4 border rounded-lg"
+              placeholder="Email Address"
+            />
+
+            <p className="font-bold">
+              By clicking "Continue", I agree that Opoko can keep me informed by
+              sending personalized emails about products and services. See our
+              Privacy Policy for details or to opt-out at any time.
+            </p>
+
+            <div className="flex items-center gap-4">
+              <input type="checkbox" id="confirm" />
+              <label htmlFor="confirm">Please don't contact me via email</label>
+            </div>
+          </div>
+          <button className="px-16 py-4 rounded-2xl bg-color-blue text-white text-center disabled:opacity-50 disabled:cursor-not-allowed">
+            Continue
+          </button>
+        </form>
+      </section>
+      <section>
+
+      </section>
     </div>
   );
 }

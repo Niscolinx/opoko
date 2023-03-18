2973674a77300fe01ee190b62ba39b73d64c6a2e
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 11:14:00 +0100

    working on pricing

--- src/pages/Billing.tsx
+++ src/pages/Billing.tsx
@@ -3,7 +3,17 @@ import React, { useState } from 'react';
 function Billing() {
   const [step, setStep] = useState(1);
 
-  const renderPaths = new Map<number, JSX.Element>([[1, <></>]]);
+  const renderPaths = new Map<number, JSX.Element>([
+    [
+      1,
+      <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel alias
+        distinctio illum cum? Asperiores reiciendis voluptatibus fuga repellat
+        et! Incidunt quasi placeat ratione! Minus excepturi fugiat incidunt
+        voluptate amet magni.
+      </>,
+    ],
+  ]);
 
   return <div>Billing</div>;
 }

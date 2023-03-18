4e567213b63dffc279b07c0111a194e96d739ea7
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 13:17:08 +0100

    working on pricing

--- src/container/Auth.tsx
+++ src/container/Auth.tsx
@@ -43,13 +43,6 @@ function Auth() {
   const path = pathSignal.value;
   const dialogRef = useRef<HTMLDialogElement | null>(null);
 
-  effect(() => {
-    console.log('path signal', pathSignal.value)
-    if(pathSignal.value){
-      
-    }
-  })
-
   const closeDialog = () => {
     if (dialogRef.current) {
       dialogRef.current.close();
@@ -61,6 +54,13 @@ function Auth() {
       dialogRef.current.showModal();
     }
   };
+
+  effect(() => {
+    console.log('path signal', pathSignal.value);
+    if (pathSignal.value) {
+      openDialog();
+    }
+  });
 
   const renderAuthPath = new Map<number, JSX.Element>([
     [1, path === 'signup' ? <FirstSignUpStep /> : <FirstLoginStep />],

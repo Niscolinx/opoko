56f04771524ac707b4ddcfdbcc69324fb4e42eeb
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 10:44:37 +0100

    working on about us

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -12,8 +12,11 @@ export const Card = ({ title, description, list, amount }: Required<Card>) => {
       className={`h-[40rem] max-w-[27rem] grid gap-8 justify-start  text-white px-[2rem] py-[4rem] $`}
     >
       <p className="font-medium text-[3rem]">{title}</p>
+      <p className='font-bold text-[5rem]'>${amount.toLocaleString()}</p>
       <p className="text-[1.4rem] ">{description}</p>
-      <p className="text-[3rem] mt-auto">{Icon}</p>
+      <div>
+        
+      </div>
     </div>
   );
 };

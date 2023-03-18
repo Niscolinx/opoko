8f809d6df2cc2548d74fdeb4106ed5975bff4ec1
Igboanugwo Collins <munisco12@gmail.com>
2023-03-18 12:25:11 +0100

    working on pricing

--- src/pages/billing/Billing.tsx
+++ src/pages/billing/Billing.tsx
@@ -7,7 +7,7 @@ function Billing() {
   const renderPaths = new Map<number, JSX.Element>([[1, <First />]]);
 
   return (
-    <div className="grid gap-4 h-screen overflow-hidden p-[8rem]">
+    <div className="grid gap-4 h-screen overflow-hidden p-[8rem] content-start">
       <p>
         Step {step} of {renderPaths.size}
       </p>

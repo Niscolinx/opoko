78960ea516d33bd99056c3c429de149aa3d0148b
Igboanugwo Collins <munisco12@gmail.com>
2023-03-19 06:58:14 +0100

    could not resolve @preact/signals problem with vite build yet

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['preact'],
-    },
-    commonjsOptions: {
-      esmExternals: ['preact'],
-    },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['preact'],
+  //   },
+  //   commonjsOptions: {
+  //     esmExternals: ['preact'],
+  //   },
   },
   resolve: {
     // alias: [

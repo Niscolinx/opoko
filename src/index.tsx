502d270bcfcaf4dd6710fa3cdb3334a680e73703
Igboanugwo Collins <munisco12@gmail.com>
2023-03-19 07:00:01 +0100

    could not resolve @preact/signals problem with vite build yet

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './Entry/App';
-import './index.css'
-import './sass/main.scss';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-      <App />
-);
